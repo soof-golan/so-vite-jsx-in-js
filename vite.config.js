3a5db89e2c9f05e2568301b4e50cc9265ca28ed7
Soof Golan <soofgolan@gmail.com>
2023-03-24 15:16:46 +0300

    configure vite to allow JSX in .js and TSX in .ts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,32 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: "tsx",
+    include: [
+      // Business as usual for .jsx and .tsx files
+      "src/**/*.jsx",
+      "src/**/*.tsx",
+      "node_modules/**/*.jsx",
+      "node_modules/**/*.tsx",
+
+      // Add the specific files you want to allow JSX syntax in
+      "src/LocalJsxInJsComponent.js",
+      "node_modules/bad-jsx-in-js-component/index.js",
+      "node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js",
+      "node_modules/bad-jsx-in-js-component/ts/index.ts",
+      "node_modules/bad-jsx-in-js-component/ts/BadJSXinTS.ts",
+
+      // --- OR ---
+
+      // Add these lines to allow all .js files to contain JSX
+      "src/**/*.js",
+      "node_modules/**/*.js",
+
+      // Add these lines to allow all .ts files to contain JSX
+      "src/**/*.ts",
+      "node_modules/**/*.ts",
+    ],
+    exclude: [],
+  }
 })
