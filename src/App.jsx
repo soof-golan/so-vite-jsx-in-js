be1d45ad93e5f86252a7eb8148327769847e88ba
Soof Golan <soofgolan@gmail.com>
2023-03-24 15:16:13 +0300

    this is broken op purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import BadJSXinJS from "bad-jsx-in-js-component/js/BadJSXinJS";
+import GoodJSX from "bad-jsx-in-js-component/js/GoodJSX";
+import BadTSXInTs from "bad-jsx-in-js-component/ts";
+import GoodTSX from "bad-jsx-in-js-component/ts/GoodTSX";
+import LocalJsxInJsComponent from "./LocalJsxInJsComponent.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <LocalJsxInJsComponent />
+      <BadJSXinJS />
+      <GoodJSX />
+      <BadTSXInTs />
+      <GoodTSX />
     </div>
   )
 }
